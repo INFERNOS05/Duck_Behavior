770ca707cb7868e7b9ed70b47f7547c7df2e2bef
sehajdeep <155955772+INFERNOS05@users.noreply.github.com>
2025-04-21 00:48:29 +0530

    Update Drown.java

--- Drown.java
+++ Drown.java
@@ -1,1 +1,15 @@
+// Drown.java
+
+// This class implements the SwimBehavior interface.
+// It represents a specific swimming behavior where the entity "drowns" instead of swimming.
+public class Drown implements SwimBehavior {
+    
+    // The swim() method provides the implementation of the swim behavior.
+    // In this case, it simulates the behavior of drowning.
+    @Override
+    public void swim() {
+        // Printing a message that represents the drowning action
+        System.out.println("Oh! I'm drowning!");
+    }
+}
 
