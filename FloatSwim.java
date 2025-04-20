32c0be1f842debde07d77d37d37c118b2cb69367
sehajdeep <155955772+INFERNOS05@users.noreply.github.com>
2025-04-21 00:49:06 +0530

    Update FloatSwim.java

--- FloatSwim.java
+++ FloatSwim.java
@@ -1,1 +1,15 @@
+// FloatSwim.java
+
+// This class implements the SwimBehavior interface.
+// It defines a specific swimming behavior where the entity can only float, but cannot swim.
+public class FloatSwim implements SwimBehavior {
+    
+    // The swim() method provides the implementation of the swim behavior.
+    // In this case, it simulates floating on water rather than actual swimming.
+    @Override
+    public void swim() {
+        // Printing a message that represents the floating action
+        System.out.println("I can only float :(");
+    }
+}
 
