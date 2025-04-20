2dcb201116b94fb5953b16578803a290f90659ff
sehajdeep <155955772+INFERNOS05@users.noreply.github.com>
2025-04-21 00:47:38 +0530

    Update Swim.java

--- Swim.java
+++ Swim.java
@@ -1,1 +1,15 @@
+// Swim.java
+
+// This class implements the SwimBehavior interface, defining a specific behavior for swimming.
+// The Swim class represents the action of swimming, fulfilling the contract specified by SwimBehavior interface.
+public class Swim implements SwimBehavior {
+    
+    // The swim() method provides the implementation of the swim behavior.
+    // This method outputs a message indicating the entity can swim.
+    @Override
+    public void swim() {
+        // Printing a message that represents the swimming action
+        System.out.println("I can swim.");
+    }
+}
 
