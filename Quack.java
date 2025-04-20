07df9d2e67d2b0999639fe4fcee764bcc7c893b3
sehajdeep <155955772+INFERNOS05@users.noreply.github.com>
2025-04-21 00:51:07 +0530

    Update Quack.java

--- Quack.java
+++ Quack.java
@@ -1,1 +1,15 @@
+// Quack.java
+
+// This class implements the QuackBehavior interface.
+// It represents a specific quacking behavior where the entity can quack normally.
+public class Quack implements QuackBehavior {
+    
+    // The quack() method provides the implementation of the quack behavior.
+    // In this case, the entity quacks normally, so it outputs a "Quack!" sound.
+    @Override
+    public void quack() {
+        // Printing a message that represents a normal quacking sound
+        System.out.println("Quack! Quack!");
+    }
+}
 
