6217f6420bbb3675d6ae3539404819a952a5b0cd
sehajdeep <155955772+INFERNOS05@users.noreply.github.com>
2025-04-21 00:53:58 +0530

    Update SwimBehavior.java

--- SwimBehavior.java
+++ SwimBehavior.java
@@ -1,1 +1,11 @@
+// SwimBehavior.java
+
+// This is the SwimBehavior interface, which defines the contract for any swimming behavior.
+// Any class implementing this interface must provide a specific implementation for the swim() method.
+public interface SwimBehavior {
+    
+    // The swim() method is intended to define the swimming behavior of the class that implements this interface.
+    // The method must be implemented to perform the swimming action.
+    void swim();
+}
 
