ccbd2eb7db7b08527a1856f56fa2509f66e75ce4
sehajdeep <155955772+INFERNOS05@users.noreply.github.com>
2025-04-21 00:49:30 +0530

    Update FlyBehavior.java

--- FlyBehavior.java
+++ FlyBehavior.java
@@ -1,1 +1,11 @@
+// FlyBehavior.java
+
+// This is the FlyBehavior interface, which defines the contract for any flying behavior.
+// Any class implementing this interface must provide a specific implementation for the fly() method.
+public interface FlyBehavior {
+    
+    // The fly() method is intended to define the flying behavior of the class that implements this interface.
+    // The method must be implemented to perform the flying action.
+    void fly();
+}
 
