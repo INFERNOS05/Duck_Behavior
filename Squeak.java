3ebf96cf5d641f1623f52e2ae9c9ef2b0d44c54d
sehajdeep <155955772+INFERNOS05@users.noreply.github.com>
2025-04-21 00:53:29 +0530

    Update Squeak.java

--- Squeak.java
+++ Squeak.java
@@ -1,1 +1,15 @@
+// Squeak.java
+
+// This is the Squeak class. It implements the QuackBehavior interface.
+// It represents a specific quacking behavior where the entity makes a squeaking sound.
+public class Squeak implements QuackBehavior {
+    
+    // The quack() method provides the implementation of the quack behavior.
+    // In this case, the entity squeaks, so it outputs a "Squeak!" sound.
+    @Override
+    public void quack() {
+        // Printing a message that represents the squeaking sound
+        System.out.println("Squeak! Squeak!");
+    }
+}
 
